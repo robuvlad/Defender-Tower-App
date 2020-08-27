63afbb2f2b17aa79cc3b16164434d8f1c18b1d6c
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-08-27 14:02:43 +0300

    added bottom buttons for swithing between defenders

---
+++ Assets/Scripts/Ally/DefenderSelection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSelection : MonoBehaviour
+{
+    [SerializeField] GameObject defender = null;
+
+    private void OnMouseDown()
+    {
+        var defenders = FindObjectsOfType<DefenderSelection>();
+        foreach(DefenderSelection defender in defenders)
+        {
+            defender.GetComponent<SpriteRenderer>().color = new Color32(38, 38, 38, 255);
+        }
+        GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+        PlaceDefender(defender);
+    }
+
+    private void PlaceDefender(GameObject def)
+    {
+        var positionDefs = FindObjectsOfType<PositionDefenders>();
+        foreach(PositionDefenders pos in positionDefs)
+        {
+            pos.SetDefender(def);
+        }
+    }
+}
--- Assets/Scripts/Ally/PositionDefenders.cs
+++ Assets/Scripts/Ally/PositionDefenders.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Ally;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PositionDefenders : MonoBehaviour
 {
-    [SerializeField] GameObject defender = null;
+    private GameObject defender = null;
 
     private Vector3 SPAWNING_POINT = new Vector3(0, 0, -1);
 
@@ -15,6 +16,15 @@ public class PositionDefenders : MonoBehaviour
 
     private void SpawnDefender()
     {
-        GameObject newDefender = Instantiate(defender, transform.position + SPAWNING_POINT, Quaternion.identity) as GameObject;
+        if (defender != null)
+        {
+            GameObject newDefender = Instantiate(defender, transform.position + SPAWNING_POINT, Quaternion.identity) as GameObject;
+            Destroy(GetComponent<BoxCollider2D>());
+        }
+    }
+
+    public void SetDefender(GameObject def)
+    {
+        this.defender = def;
     }
 }
