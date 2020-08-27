8f4d2081e25d2c4c4224a25acca38e8c67e011ad
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-08-27 12:08:45 +0300

    resolved velocity bug

---
+++ Assets/Scripts/Ally/PositionDefenders.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionDefenders : MonoBehaviour
+{
+    [SerializeField] GameObject defender = null;
+
+    private Vector3 SPAWNING_POINT = new Vector3(0, 0, -1);
+
+    private void OnMouseDown()
+    {
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, transform.position + SPAWNING_POINT, Quaternion.identity) as GameObject;
+    }
+}
