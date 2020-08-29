fb712223f74ea2d2e87c26571b2ac8e56c155fdf
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-08-29 21:31:29 +0300

    added healthbar color option

--- Assets/Scripts/HealthBar/HealthBar.cs
+++ Assets/Scripts/HealthBar/HealthBar.cs
@@ -8,30 +8,57 @@ public class HealthBar : MonoBehaviour
     [SerializeField] Enemy enemy = null;
     [SerializeField] GameObject bar = null;
 
-    private float health;
+    private GameObject barSprite = null;
+
+    private byte red = 0;
+    private byte  green = 255;
+    private byte blue = 0;
+
+    private float maxHealth;
 
     void Start()
     {
-        health = enemy.GetHealth();
+        maxHealth = enemy.GetHealth();
+        barSprite = bar.transform.GetChild(0).gameObject;
     }
 
     void Update()
     {
-        ShowHealthBar();
+        float health = enemy.GetHealth();
+        ShowHealthBar(health);
+        ChangeColorBasedOnHealth(health);
         Debug.Log(enemy.GetHealth());
     }
 
-    private void ShowHealthBar()
+    private void ShowHealthBar(float health)
     {
-        if (enemy.GetHealth() >= 0.0f)
+        if (health >= 0.0f)
         {
-            float normalizedHealth = NormalizedHealth(enemy.GetHealth());
+            float normalizedHealth = NormalizedHealth(health);
             bar.transform.localScale = new Vector2(normalizedHealth, 1.0f);
         }
     }
 
     private float NormalizedHealth(float actualHealth)
     {
-        return actualHealth / health;
+        return actualHealth / maxHealth;
+    }
+
+    private void ChangeColorBasedOnHealth(float health)
+    {
+        NormalizedColor(health);
+        var sprite = barSprite.GetComponent<SpriteRenderer>() as SpriteRenderer;
+        sprite.color = new Color32(red, green, blue, 255);
+    }
+
+    private void NormalizedColor(float health)
+    {
+        if (health >= maxHealth / 2)
+        {
+            red = (byte)((255 - (health / maxHealth) * 255) * 2);
+        } else
+        {
+            green = (byte)((health / maxHealth) * 255 * 2);
+        }
     }
 }
