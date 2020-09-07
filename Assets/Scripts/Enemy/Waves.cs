30cbb8cb9c409a99cfa9f773a254556de5367b70
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-09-07 18:11:13 +0300

    added wave panel

--- Assets/Scripts/Enemy/Waves.cs
+++ Assets/Scripts/Enemy/Waves.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Waves : MonoBehaviour
 {
+    [Header("Configurations")]
     [SerializeField] List<Wave> waves = null;
     [SerializeField] float delayTime;
+
+    [Header("Wave Panel")]
+    [SerializeField] GameObject wavePanel = null;
+    [SerializeField] float panelTime = 0.0f;
 
     private int noOfWaves;
 
     void Start()
     {
+        wavePanel.SetActive(false);
         noOfWaves = waves.Count;
         StartCoroutine(InstantiateNewWave());
     }
@@ -19,11 +26,42 @@ public class Waves : MonoBehaviour
     {
         while(noOfWaves > 0)
         {
+            StartCoroutine(ShowWavePanel(waves.Count - noOfWaves + 1));
             Wave currentWave = waves[waves.Count - noOfWaves];
             float totalTime = currentWave.GetNoOfEnemies() * currentWave.GetTimeBetweenEnemies() + delayTime;
             StartCoroutine(currentWave.InstantiateRandomEnemies());
             noOfWaves -= 1;
             yield return new WaitForSeconds(totalTime);
         }
+    }
+
+    private IEnumerator ShowWavePanel(int currentWave)
+    {
+        wavePanel.transform.GetChild(0).GetComponent<Text>().text = "Wave " + currentWave + "/" + waves.Count;
+        wavePanel.SetActive(true);
+        yield return new WaitForSeconds(panelTime);
+        wavePanel.SetActive(false);
+        yield return new WaitForSeconds(panelTime);
+        wavePanel.SetActive(true);
+        yield return new WaitForSeconds(panelTime);
+        wavePanel.SetActive(false);
+        yield return new WaitForSeconds(panelTime);
+        wavePanel.SetActive(true);
+        yield return new WaitForSeconds(panelTime);
+        wavePanel.SetActive(false);
+        yield return new WaitForSeconds(panelTime);
+        wavePanel.SetActive(true);
+        yield return new WaitForSeconds(panelTime);
+        wavePanel.SetActive(false);
+    }
+
+    public float GetDelayTime()
+    {
+        return delayTime;
+    }
+
+    public List<Wave> GetWaves()
+    {
+        return waves;
     }
 }
---
+++ Assets/Scripts/Gameplay/Gametimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gametimer : MonoBehaviour
+{
+    [SerializeField] float extraTime = 0.0f;
+
+    private Slider slider = null;
+    private float totalTime;
+
+    void Start()
+    {
+        SetupTimer();
+    }
+
+    void Update()
+    {
+        UpdateSliderValue();
+    }
+
+    private void SetupTimer()
+    {
+        slider = GetComponent<Slider>() as Slider;
+        slider.enabled = false;
+        totalTime = extraTime;
+        Waves waves = FindObjectOfType<Waves>() as Waves;
+        foreach (Wave wave in waves.GetWaves())
+        {
+            totalTime += wave.GetNoOfEnemies() * wave.GetTimeBetweenEnemies();
+        }
+        totalTime += waves.GetDelayTime();
+    }
+
+    private void UpdateSliderValue()
+    {
+        slider.value = Time.timeSinceLevelLoad / totalTime;
+    }
+}
