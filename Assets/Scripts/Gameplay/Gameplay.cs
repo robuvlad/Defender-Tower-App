f41780bdfeb5cdbd41798469d91d980a551c222c
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-09-07 13:19:00 +0300

    added pause button sprite & pause logic

---
+++ Assets/Scripts/Gameplay/Gameplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    public void PauseOrContinue()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+    }
+}
