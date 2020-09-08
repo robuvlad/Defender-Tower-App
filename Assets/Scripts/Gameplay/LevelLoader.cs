1d5e4c0b2f313265fb8194f5e9beab18699634c2
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-09-08 13:38:03 +0300

    added splash screen & menu

---
+++ Assets/Scripts/Gameplay/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float timeToWait = 0.0f;
+
+    private int sceneIndex;
+
+    void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (sceneIndex == 0)
+        {
+            StartCoroutine(WaitAndContinue());
+        }
+    }
+
+    private IEnumerator WaitAndContinue()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        sceneIndex += 1;
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
