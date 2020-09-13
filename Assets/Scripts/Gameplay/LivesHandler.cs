9ce88a901dea76929bbbf8cc5b7c1b1901de2418
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-09-13 13:18:30 +0300

    added lives handler

---
+++ Assets/Scripts/Gameplay/LivesHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesHandler : MonoBehaviour
+{
+    [SerializeField] int totalLives = 0;
+    [SerializeField] Text livesText = null;
+
+    void Update()
+    {
+        livesText.text = totalLives.ToString();
+    }
+
+    public float GetTotalLives()
+    {
+        return totalLives;
+    }
+
+    public void SetLives(int lives)
+    {
+        this.totalLives = lives;
+    }
+
+    public void DecreaseLives(int lives)
+    {
+        if (totalLives >= lives)
+        {
+            totalLives -= lives;
+        }
+    }
+}
---
+++ Assets/Scripts/Shaders/Gate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    private const string ENEMY_TAG = "enemy";
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == ENEMY_TAG)
+        {
+            Destroy(other.gameObject);
+            CheckForLives();
+        }
+    }
+
+    private void CheckForLives()
+    {
+        var lives = FindObjectOfType<LivesHandler>();
+        lives.DecreaseLives(1);
+        if (lives.GetTotalLives() <= 0)
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+}
