5cb18ceb3277d9720eb6ebed007925bea19d5515
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-09-13 12:38:01 +0300

    added points text when enemies die

--- Assets/Scripts/Enemy/EnemyAttacked.cs
+++ Assets/Scripts/Enemy/EnemyAttacked.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyAttacked : MonoBehaviour
 {
+    [Header("Config")]
     [SerializeField] float timeToDie = 1.0f;
+
+    [Header("UI Points Text")]
+    [SerializeField] GameObject pointsText = null;
+    [SerializeField] float pointsTime = 0.0f;
 
     private Enemy enemy = null;
     private PointsHandler points = null;
@@ -31,9 +37,16 @@ public class EnemyAttacked : MonoBehaviour
                 var animator = GetComponent<Animator>();
                 animator.SetBool(IS_DYING_ANIMATOR_PARAM, true);
                 points.IncreasePoints(enemy.GetPoints());
+                ShowPoints();
                 Destroy(this.gameObject, timeToDie);
             }
             Destroy(other.gameObject);
         }
+    }
+
+    private void ShowPoints()
+    {
+        var pointsT = Instantiate(pointsText, gameObject.transform.position, Quaternion.identity);
+        Destroy(pointsT, pointsTime);
     }
 }
