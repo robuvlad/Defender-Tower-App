98abfc0ebad2c9623c773c08194c30e1ab99d48d
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-09-14 17:34:59 +0300

    disabled level button in menu

---
+++ Assets/Scripts/Gameplay/MenuButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField] List<Button> buttons = null;
+
+    private Color32 disabledColorButton = new Color32(255, 255, 255, 100);
+    private Color32 disabledColorText = new Color32(23, 32, 36, 100);
+
+
+    void Start()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        if (index == 1)
+        {
+            DisableButtons();
+        }
+    }
+
+    private void DisableButtons()
+    {
+        int currentLevel = PlayerPrefsController.GetLevelPrefs();
+        for(int i = currentLevel + 1; i < buttons.Count; i++)
+        {
+            buttons[i].enabled = false;
+            ChangeButtonColor(buttons[i]);
+        }
+    }
+
+    private void ChangeButtonColor(Button button)
+    {
+        button.GetComponent<Image>().color = disabledColorButton;
+        foreach(Transform child in button.transform)
+        {
+            child.GetComponent<Text>().color = disabledColorText;
+        }
+    }
+
+}
--- Assets/Scripts/PlayerPrefs/PlayerPrefsController.cs
+++ Assets/Scripts/PlayerPrefs/PlayerPrefsController.cs
@@ -8,6 +8,8 @@ public class PlayerPrefsController : MonoBehaviour
     private const float MIN_VOLUME = 0.0f;
     private const float MAX_VOLUME = 1.0f;
 
+    private const string LEVEL_KEY = "Level";
+
     public static void SetVolumePrefs(float volume)
     {
         if (volume >= MIN_VOLUME && volume <= MAX_VOLUME)
@@ -19,5 +21,18 @@ public class PlayerPrefsController : MonoBehaviour
     public static float GetVolumePrefs()
     {
         return PlayerPrefs.GetFloat(VOLUME_KEY);
+    }
+
+    public static void SetLevelPrefs(int level)
+    {
+        if (level >= 0)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY, level);
+        }
+    }
+
+    public static int GetLevelPrefs()
+    {
+        return PlayerPrefs.GetInt(LEVEL_KEY);
     }
 }
