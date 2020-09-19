9993d5a45e2614f5ec19252508eaaa83c3e325df
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-09-19 17:01:56 +0300

    added store scene & score logic impl

---
+++ Assets/Scripts/Gameplay/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] List<Image> images = null;
+
+    private int score = 0;
+
+    public void IncreaseScore(int number)
+    {
+        score += number;
+    }
+
+    public void DecreaseScore(int number)
+    {
+        if (score >= number)
+        {
+            score -= number;
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void SetScore(int s)
+    {
+        score = s;
+    }
+}
